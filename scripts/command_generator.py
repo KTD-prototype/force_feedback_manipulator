59e9e0cfaf53136057dd83d7187e3881f7fef8e6
KTD-prototype desktop <ktd.prototype@gmail.com>
2019-12-28 02:43:58 +0900

    implemented master-slave robotic hand

--- scripts/command_generator.py
+++ scripts/command_generator.py
@@ -5,13 +5,19 @@ import serial
 import time
 import rospy
 from std_msgs.msg import Int16
-from sensor_msgs.msg import Joy
 from kondo_b3mservo_rosdriver.msg import Multi_servo_command
+from kondo_b3mservo_rosdriver.msg import Multi_servo_info
 
-
+# global parameters for general information
 initial_process_flag = 1
 initial_setparam_flag = 1
 the_number_of_servo = 0
+
+# global parameters for servo info
+servo_angle = [0] * 2
+servo_current = [0] * 2
+
+# global parameters for servo command
 target_position = []
 target_velocity = []
 target_torque = []
@@ -21,6 +27,7 @@ target_position_by_torque = []
 # callback function called when the number of servos under control is informed
 def callback_init(number):
     global the_number_of_servo
+    global servo_angle, servo_current
     the_number_of_servo = number.data  # get data
 
     # prepare paramters for servo command
@@ -30,51 +37,59 @@ def callback_init(number):
         target_torque.append(0)
         target_position_by_torque.append(0)
 
+    print(servo_current)
 
+
+# callback function to get informations of servos
 def callback_get_servo_info(multi_servo_info):
-    global target_position, target_velocity, target_torque, target_position_by_torque
-    global the_number_of_servo, initial_process_flag
+    global servo_angle, servo_current
+    servo_angle = multi_servo_info.motor_position
+    servo_current = multi_servo_info.motor_current
+
+
+# function to control servos
+def control_manipulator(angle, torque):
+    global the_number_of_servo
+
+    # setup an instance to publish servo command
     multi_servo_command = Multi_servo_command()
 
-    for i in range(num):
-        target_position[i] = joy_msg.axes[0] * 32000  # left stick LR
-        target_velocity[i] = joy_msg.axes[3] * 32767  # right stick LR
-        target_torque[i] = joy_msg.axes[1] * 7000  # left stick FB
-        # right sitck FR
-        target_position_by_torque[i] = joy_msg.axes[0] * 32000
-        if target_position_by_torque[i] > 16000:
-            target_position_by_torque[i] = 16000
-        elif target_position_by_torque[i] < -16000:
-            target_position_by_torque[i] = -16000
+    # parameters for motion
+    angle_command_slave = 0
+    torque_command_slave = 0
 
-        multi_servo_command.target_position.append(target_position[i])
-        multi_servo_command.target_velocity.append(target_velocity[i])
-        multi_servo_command.target_torque.append(target_torque[i])
-        multi_servo_command.target_position_by_torque.append(
-            target_position_by_torque[i])
-    pub.publish(multi_servo_command)
-    del multi_servo_command
+    # modify command value to avoid over current and other harmful motion
+    if angle[0] < 0:
+        angle_command_slave = 0
+    elif angle[0] > 9000:
+        angle_command_slave = 9000
+    else:
+        angle_command_slave = angle[0]
 
+    # store servo command into the instance of ROS message
+    multi_servo_command.target_torque = [0, 0]
+    multi_servo_command.target_position_by_torque = [0, angle_command_slave]
 
-def control_manipulator():
-    aa
-    aA
+    # publish ROS message to command servos
+    multi_servo_command_pub.publish(multi_servo_command)
 
 
 if __name__ == '__main__':
+    # global servo_angle, servo_current
     rospy.init_node('generate_multi_command')
 
     rospy.Subscriber('the_number_of_servo', Int16, callback_init, queue_size=1)
     rospy.Subscriber('multi_servo_info', Multi_servo_info, callback_get_servo_info, queue_size=1)
     # rospy.Subscriber('joy', Joy, callback_generate_multi_command, queue_size=1)
-    pub = rospy.Publisher('multi_servo_command',
-                          Multi_servo_command, queue_size=1)
+    multi_servo_command_pub = rospy.Publisher('multi_servo_command',
+                                              Multi_servo_command, queue_size=1)
 
     rate = rospy.Rate(100)
 
     while not rospy.is_shutdown():
         try:
-            control_manipulator()
+            # control servos based on the information of them
+            control_manipulator(servo_angle, servo_current)
 
         except IOError:
             pass
